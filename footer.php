ed5f26cdebad1b92d7f276f063cdd4302a70f372
bfy0412 <642731336@qq.com>
2015-08-13 17:02:38 +0800

    cut back the spacing of the word

--- footer.php
+++ footer.php
@@ -1,10 +1,8 @@
 		<section id="footer">
 			<p>
 				Powerd by
-				<a href="http://wordpress.org/">WordPress</a>| Designed by| <a href="/">huaisu</a>
-				<br>
-					Copyright© 2015-2021 怀素
-				<br><!--版权信息-->
+				<a href="http://wordpress.org/">WordPress</a>| Designed by&nbsp;| <a href="/">huaisu</a>
+				<p>Copyright© 2015-2021 怀素</p><!--版权信息-->
 				<a href="http://www.miibeian.gov.cn/" target="_blank">渝ICP备15006198号</a>
 			</p>
 		</section>
