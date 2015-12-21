0ee4c618c5365cf997f4f72100547feadf9d9b26
bfy0412 <642731336@qq.com>
2015-12-21 21:42:18 +0800

    modified the font-size of footer msg

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 		<section id="footer">
 			<p>
 				Powerd by
-				<a href="http://wordpress.org/">WordPress</a>| Designed by&nbsp;| <a href="/">huaisu</a>
+				<a href="http://wordpress.org/">WordPress</a>| Designed by&nbsp;| <a href="/">bifangya</a>
 				<p>Copyright© 2015-2021 怀素</p><!--版权信息-->
 				<a href="http://www.miibeian.gov.cn/" target="_blank">渝ICP备15006198号</a>
 			</p>
